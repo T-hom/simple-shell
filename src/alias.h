08288d35c830ae522cb094c84ba483df30b7e0f2
T-hom <79807260+T-hom@users.noreply.github.com>
2026-02-04 16:58:42 +0000

    Remove redundant struct definition for Alias

--- src/alias.h
+++ src/alias.h
@@ -1,11 +1,6 @@
 #define MAX_ALIASES 10 // max of 10 like pdf says
 
 typedef struct {
-    char *name;    // alias name
-    char *command; // the actual command like cd, ls, etc
-} Alias;
-
-typedef struct {
     char *alias[MAX_ALIASES][2]; // 2D array alais[i][name][command]
     int count;                // current number of aliases set
 } AliasList;
@@ -13,4 +8,5 @@ typedef struct {
 void add_alias(AliasList *alias_list, char *name, char *command);
 void remove_alias(AliasList *alias_list, char *name);
 void init_aliases(AliasList *alias_list); //  neede because otherwise it has random data
+
 void print_aliases(AliasList *alias_list);
